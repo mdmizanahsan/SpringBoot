96f66157b722991be923cf595499b5fc9b4427d2
Md Mizan Ahsan <work.mdmizanahsan@gmail.com>
2025-08-25 19:30:36 +0530

    RESTAPI: This is my first spring boot project

---
+++ student/src/main/java/com/mizan/student/Controller/StudentController.java
@@ -0,0 +1,43 @@
+package com.mizan.student.Controller;
+
+import com.mizan.student.Entity.Student;
+import com.mizan.student.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    // GET - fetch all student
+     @GetMapping
+    public List<Student> getallStudents()
+    {
+        return studentService.getallStudents();
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student)
+    {
+        return studentService.addStudent(student);
+    }
+
+    // PUT - update
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable Long id ,@RequestBody Student student)
+    {
+        return studentService.updateStudent(student, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Student deleteStudent(@PathVariable Long id)
+    {
+        return studentService.deleteStudent(id);
+    }
+}
